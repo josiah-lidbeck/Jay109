fb622977dedffda2c23ff6a83beb1cc43b63b312
josiah-lidbeck <josiahlidbeck@gmail.com>
2026-03-09 16:54:27 +0000

    Mon, Mar 9, 2026, 9:54 AM -07:00

--- script/calculator.js
+++ script/calculator.js
@@ -1,33 +1,5 @@
-let answerBox = document.querySelector(".answerBox");
-
-let numbers = document.querySelectorAll(".number");
-let operations = document.querySelectorAll(".operation");
-
-let firstNumber = "";
-let operator = "";
-
-numbers.forEach(function(button) {
-
-    button.addEventListener("click", function() {
-
-        answerBox.innerText = button.innerText;
-
-    });
-
-});
-
-operations.forEach(function(button) {
-
-    button.addEventListener("click", function() {
-
-        if(button.innerText === "+" || button.innerText === "x"){
-
-            firstNumber = answerBox.innerText;
-            operator = button.innerText;
-            answerBox.innerText = "";
-
-        }
-
-    });
-
-});
+const numbers = document.querySelectorAll(".number");
+const addButton = document.querySelector(".add");
+const multiplyButton = document.querySelector(".multiply");
+const equalButton = document.querySelector(".equalSign");
+const answerBox = document.querySelector(".answerBox");

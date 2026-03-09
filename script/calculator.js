8ef1e0bdf167f2f8813aeb3aaeed062635a254f6
josiah-lidbeck <josiahlidbeck@gmail.com>
2026-03-09 17:01:22 +0000

    Mon, Mar 9, 2026, 10:01 AM -07:00

--- script/calculator.js
+++ script/calculator.js
@@ -18,3 +18,122 @@ answerBox = document.querySelector('.answerBox')
 firstNumber = ""
 secondNumber = ""
 operation = ""
+
+
+oneBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 1
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 1
+    answerBox.innerText = secondNumber
+}
+
+})
+twoBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 2
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 2
+    answerBox.innerText = secondNumber
+}
+
+})
+threeBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 3
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 3
+    answerBox.innerText = secondNumber
+}
+
+})
+fourBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 4
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 4
+    answerBox.innerText = secondNumber
+}
+
+})
+fiveBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 5
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 5
+    answerBox.innerText = secondNumber
+}
+
+})
+sixBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 6
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 6
+    answerBox.innerText = secondNumber
+}
+
+})
+sevenBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 7
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 7
+    answerBox.innerText = secondNumber
+}
+
+})
+eightBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 8
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 8
+    answerBox.innerText = secondNumber
+}
+
+})
+nineBox.addEventListener('click', () => {
+
+if (firstNumber == null) {
+    firstNumber = 9
+    answerBox.innerText = firstNumber
+} else {
+    secondNumber = 9
+    answerBox.innerText = secondNumber
+}
+
+})
+addBox.addEventListener('click', () => {
+operation = "add"
+})
+
+multiplyBox.addEventListener('click', () => {
+operation = "multiply"
+})
+equalBox.addEventListener('click', () => {
+
+if (operation == "add") {
+    answerBox.innerText = firstNumber + secondNumber
+}
+
+if (operation == "multiply") {
+    answerBox.innerText = firstNumber * secondNumber
+}
+
+})

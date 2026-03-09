15745ae94ae39a7057c7a8334d857c3214cd1860
josiah-lidbeck <josiahlidbeck@gmail.com>
2026-03-09 16:46:42 +0000

    Mon, Mar 9, 2026, 9:46 AM -07:00

--- script/calculator.js
+++ script/calculator.js
@@ -1,9 +1,33 @@
-termOne = document.querySelectorAll(".number")
-
 let answerBox = document.querySelector(".answerBox");
 
+let numbers = document.querySelectorAll(".number");
+let operations = document.querySelectorAll(".operation");
 
-termOne.addEventListener('click', () => {
+let firstNumber = "";
+let operator = "";
 
+numbers.forEach(function(button) {
 
-    })
+    button.addEventListener("click", function() {
+
+        answerBox.innerText = button.innerText;
+
+    });
+
+});
+
+operations.forEach(function(button) {
+
+    button.addEventListener("click", function() {
+
+        if(button.innerText === "+" || button.innerText === "x"){
+
+            firstNumber = answerBox.innerText;
+            operator = button.innerText;
+            answerBox.innerText = "";
+
+        }
+
+    });
+
+});

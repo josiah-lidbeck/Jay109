28babe7cad688e39aedd3768f0812c7be83c2afd
josiah-lidbeck <josiahlidbeck@gmail.com>
2026-03-24 17:08:30 +0000

    Tue, Mar 24, 2026, 10:08 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -34,7 +34,7 @@ function createInsect() {
     const {x, y} = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-    insect.innerHTML = `<img src="${src}" alt="${alt}" style="transform: rotate(${Math.random()*360}deg)">`
+    insect.innerHTML = `<img src="${src}" alt="${alt}" >`
     game_container.appendChild(insect);
     insect.addEventListener('click', () => {
         catchInsect(insect);
